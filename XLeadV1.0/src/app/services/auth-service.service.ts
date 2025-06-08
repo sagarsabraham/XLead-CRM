66404793227f4bbe24ea129d00f4666b49a97b6a
abhiramp28 <abhiramp28@gmail.com>
2025-06-08 19:43:03 +0530

    commiting

--- XLeadV1.0/src/app/services/auth-service.service.ts
+++ XLeadV1.0/src/app/services/auth-service.service.ts
@@ -9,7 +9,7 @@ interface Privilege {
   providedIn: 'root'
 })
 export class AuthServiceService {
-  userId: number = 31;
+  userId: number = 4;
   privileges: Privilege[] = [];
 
   setPrivileges(privs: Privilege[]) {
